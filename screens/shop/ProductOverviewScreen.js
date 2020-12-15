1645012196fa584580a302923d30041885e267ed
Pancerzaba <marta.rozmyslowicz21@gmail.com>
2020-12-15 15:42:35 +0100

    The Product Overview Screen

---
+++ screens/shop/ProductOverviewScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+
+const ProductOverviewScreen = props => {
+    return (
+        <FlatList></FlatList>
+    )
+}
+
+export default ProductOverviewScreen
+
+const styles = StyleSheet.create({})
